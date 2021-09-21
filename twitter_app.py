7067e759218978cbe573a413b0bfa61a109d6c02
riefkyarif <riefkyar.ed@protonmail.com>
2021-09-20 22:50:34 -0700

    add access token

--- twitter_app.py
+++ twitter_app.py
@@ -4,4 +4,11 @@ import requests
 import requests_oauthlib
 import json
 
+# Replace the values below with yours
+ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
+ACCESS_SECRET = 'YOUR_ACCESS_SECRET'
+CONSUMER_KEY = 'YOUR_CONSUMER_KEY'
+CONSUMER_SECRET = 'YOUR_CONSUMER_SECRET'
+my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN, ACCESS_SECRET)
+
 
