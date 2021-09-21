dd2b1801b70e84c1418bb2c62703398689fcf4bc
riefkyarif <riefkyar.ed@protonmail.com>
2021-09-20 22:52:43 -0700

    add get_tweets

--- twitter_app.py
+++ twitter_app.py
@@ -11,4 +11,12 @@ CONSUMER_KEY = 'YOUR_CONSUMER_KEY'
 CONSUMER_SECRET = 'YOUR_CONSUMER_SECRET'
 my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN, ACCESS_SECRET)
 
-
+def get_tweets():
+	url = 'https://stream.twitter.com/1.1/statuses/filter.json'
+	query_data = [('language', 'en'), ('locations', '-130,-20,100,50'),('track','#')]
+	query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
+	response = requests.get(query_url, auth=my_auth, stream=True)
+	print(query_url, response)
+	return response
+	
+	
